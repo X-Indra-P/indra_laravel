65bbf16f57433b4c7e9b43f00a59f52784449280
Indra.P <114041587+X-Indra-P@users.noreply.github.com>
2023-11-12 22:37:38 +0800

    Update PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\pegawai;
+use App\Models\Pegawai;
 
 class PegawaiController extends Controller
 {
@@ -12,7 +12,7 @@ class PegawaiController extends Controller
      */
     public function index()
     {
-        $datas = pegawai::all();
+        $datas = Pegawai::all();
 
         return view('pegawai.index', compact(
             'datas'
