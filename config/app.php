b4c0261f90ff4064aa7af7d2751214bb77413f58
Indra.P <114041587+X-Indra-P@users.noreply.github.com>
2023-11-12 23:09:12 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'pegawai' => App\Facade\Pegawai::class,                                         
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
